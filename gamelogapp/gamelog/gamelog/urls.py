4329d3f66e0dab5a506b3f9a9b2155258781f06f
daminz97 <darrendamin@gmail.com>
2021-10-30 15:13:55 -0400

    update

--- gamelogapp/gamelog/gamelog/urls.py
+++ gamelogapp/gamelog/gamelog/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('logapp/', include('logapp.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
