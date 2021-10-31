6a4248b2e23abc0b2266773c20c510dedb4ec50a
daminz97 <darrendamin@gmail.com>
2021-10-30 21:05:28 -0400

    Update game.js

--- logapp/static/logapp/js/game.js
+++ logapp/static/logapp/js/game.js
@@ -201,7 +201,7 @@ drawGraph();
 
 $(document).ready(function() {
     // get retail price
-    var previewUrl = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=C919805BD39CC41E796AFE30BACE9E33&format=json";
+    var previewUrl = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=0EE969E4B7EC0D6B60DB101D5F18AD00&format=json";
     $.ajax({
         type: 'GET',
         crossDomain: true,
